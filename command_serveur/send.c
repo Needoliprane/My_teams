2a92ccaf2c47094834df76a532dcb0f59cec601b
Needoliprane <louis-auguste.dumas@epitech.eu>
2020-05-13 22:38:30 +0200

    [update] error management, send and login

--- command_serveur/send.c
+++ command_serveur/send.c
@@ -12,9 +12,10 @@ int send_command(peer_t *peer, char **data)
 {
     int index = 2;
 
-    if (peer->username == NULL) {
+    if (peer->username == NULL)
         return (84);
-    }
+    if (data[index] == NULL)
+        return (84);
     (CHECK_SIZE) ? index++ : 0;
     send_fast(my_strcat(my_strcat("ok ! ; ", data[1]), my_strcat(" ; ", data[index])));
     if (peer->message == NULL)
--- source_client/recive.c
+++ source_client/recive.c
@@ -11,6 +11,7 @@
 int handle_received_message(message_t *message, peer_t *peer)
 {
     printf("Received message from server.\n");
-    print_message(message);
+    if (strlen(message->data) > strlen("ok !"))
+        print_message(message);
     return 0;
 }
